86e0f5ad69c4e0cc7928ac135239c3353fcd9934
artemiy <artem.pospelov2018@yandex.ru>
2022-06-12 23:26:43 +0300

    add simple cli cowsay programm

---
+++ catsay/src/main.rs
@@ -0,0 +1,46 @@
+extern crate structopt;
+extern crate colored;
+
+use structopt::StructOpt;
+use colored::*;
+
+#[derive(StructOpt)]
+struct Options {
+    #[structopt(default_value = "Meow!")]
+    /// What does the cat say?
+    message: String,
+
+    #[structopt(short = "d", long = "dead")]
+    /// Make the cat appear dead
+    dead: bool,
+
+    #[structopt(short = "f", long = "file", parse(from_os_str))]
+    /// Load the cat text picture from the specified file
+    catfile: Option<std::path::PathBuf>
+}
+
+fn main() {
+    let options = Options::from_args();
+    let message = options.message;
+    if message.to_lowercase() == "woof" {
+        eprintln!("A cat shouldn't bark like a dog.")
+    }
+    let eye = if options.dead { "x" } else { "o" };
+    match &options.catfile {
+        Some(path) => {
+            let cat_template = std::fs::read_to_string(path)
+            .expect(&format!("could not read file {:?}", path));
+            let cat_picture = cat_template.replace("{eye}", eye);
+            println!("{}", &cat_picture);
+        }
+        None =>    {
+            println!("{}", message.bright_yellow().underline().on_purple());
+            println!(" \\");
+            println!("  \\");
+            println!("     /\\_/\\");
+            println!("    ( {eye} {eye} )", eye=eye);
+            println!("    =( I )=");
+            }
+    }
+
+}
